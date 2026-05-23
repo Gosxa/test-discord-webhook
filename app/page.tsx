431687d9c3aee820c02d137de6a013ad2bf26c42
Gosxa <ihorkondrashov@gmail.com>
2026-05-23 21:46:32 +0300

    some changes

--- app/page.tsx
+++ app/page.tsx
@@ -36,7 +36,7 @@ export default function HomePage() {
         <h1>Discord Webhook Deploy Test</h1>
 
         <p>
-          This page is used to test Vercel deployment and Discord webhook notifications.
+          This page is used to test Vercel deployment and Discord webhook notifications. xD
         </p>
 
         <button onClick={sendTestNotification} disabled={loading}>
